4023a9d038ec472a1c9476ef22035e7278ad4254
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-03-20 12:51:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
-diff <( curl -s https://repo.jellyfin.org/install-debuntu.sh -o install-debuntu.sh; sha256sum install-debuntu.sh ) <( curl -s https://repo.jellyfin.org/install-debuntu.sh.sha256sum )
-less install-debuntu.sh
-sudo bash install-debuntu.sh
 
